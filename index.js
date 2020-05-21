e69f72e12b7010d792de2d4d0f6444c9709e99c6
prath2001 <pdhawan1975@gmail.com>
2020-05-22 02:38:44 +0530

    learnt to use callbacks and error handling

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ function solverect(l,b){
 
 	console.log("Solving for rectangle with l = "+l+" and b = "+b);
 
-	if(l<=0 || b<=0)
-		console.log("Rectangle dimension should be greater than 0.");
-	else
-      {	
-      	console.log("The perimeter of rectangle is "+rect.perimeter(l,b)+" .");
-		console.log("The area of rectangle is "+rect.area(l,b)+" .");
-	}
+	rect(l,b, (err, rectangle) => {
+		if(err) 
+			console.log("Error! : ", err.message);
+		else
+		{
+			console.log("The area of rectangle is "+rectangle.perimeter()+" .");
+		    console.log("The perimeter of rectangle is "+rectangle.area()+" .");
+		}
+	});
 }
-
-solverect(2,4);
+solverect(2,6);
 solverect(0,5);
 solverect(1,3);
