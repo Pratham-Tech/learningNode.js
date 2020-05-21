75a01ce79346c8ce8d9b0bff17f9e639a7529b95
prath2001 <pdhawan1975@gmail.com>
2020-05-22 01:26:27 +0530

    First program in node.

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+	perimeter: (x,y) => (2*(x+y)),
+	area: (x,y) => (x*y)
+};
+
+function solverect(l,b){
+
+	console.log("Solving for rectangle with l = "+l+" and b = "+b);
+
+	if(l<=0 || b<=0)
+		console.log("Rectangle dimension should be greater than 0.");
+	else
+      {	
+      	console.log("The perimeter of rectangle is "+rect.perimeter(l,b)+" .");
+		console.log("The area of rectangle is "+rect.area(l,b)+" .");
+	}
+}
+
+solverect(2,4);
+solverect(0,5);
---
+++ rectangle.js
@@ -0,0 +1,3 @@
+module.exports.perimeter = (x,y) => (2*(x+y));
+module.exports.area = (x,y) => (x*y);
+module.exports.sum = (x,y) => (x+y);
